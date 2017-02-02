1a113cb760aac4abde975f4e2951c9e7dfc41df2
jyun10 <yunjoe25@gmail.com>
2017-02-01 19:36:25 -0500

    2/1/2017 fragments added

---
+++ app/src/main/java/com/kjdy/josephdemo/adapter/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.kjdy.josephdemo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yunjo on 2/1/2017.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragmentList;
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    public void setContent(ArrayList<Fragment> fragmentList){
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+    }
+}
